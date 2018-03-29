ec8a9c49f922474da4a4c5aaab44dac2bf510c6f
Vo Dang Anh <alt.3dbsk@gmail.com>
2018-03-29 08:58:11 +0700

    commit 28/3 quen push

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app') 
 @section('content')
 <div class="container">
-    <h1>Creating User</h1>
+    <h1>Editing User</h1>
     @if(count($errors) > 0)
     <div class="aler alert-danger">
         @foreach($errors->all() as $err)
@@ -14,27 +14,28 @@
             {{session('thongbao')}}
         </div>
     @endif
-    <form action="/listusers" method="POST">
+    <form action="/listusers/{{$users->id}}" method="POST">
+        <input type="hidden" name="_method" value="PUT">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="col-md-6 form-group">
             <label for="">Name</label>
-            <input class="form-control" autofocus="" type="text" name="name">
+        <input class="form-control" autofocus="" type="text" name="name" value="{{$users->name}}">
         </div>
         <div class="col-md-6 form-group">
             <label for="">Address</label>
-            <input class="form-control" autofocus="" type="text" name="address">
+            <input class="form-control" autofocus="" type="text" name="address" value="{{$users->address}}">
         </div>
         <div class="col-md-6 form-group">
             <label for="">Email</label>
-            <input class="form-control" autofocus="" type="text" name="email">
+            <input class="form-control" autofocus="" type="text" name="email" value="{{$users->email}}">
         </div>
         <div class="col-md-6 form-group">
             <label for="">Password</label>
-            <input class="form-control" autofocus="" type="password" name="password">
+            <input class="form-control" autofocus="" type="password" name="password" value="{{$users->password}}">
         </div>
         <div class="col-md-6 form-group">
             <label for="">Phone</label>
-            <input class="form-control" autofocus="" type="number" name="phone">
+            <input class="form-control" autofocus="" type="number" name="phone" value="{{$users->phone}}">
         </div>
         <div class="col-md-6 form-group">
             <label for="">Role</label>
@@ -44,7 +45,7 @@
             </select>
         </div>
         <div class="col-md-6 form-group">
-            <input class="btn btn-primary" type="submit" name="create" value="CREATE">
+            <input class="btn btn-primary" type="submit" name="update" value="EDIT">
         </div>
     </form>
 </div>
